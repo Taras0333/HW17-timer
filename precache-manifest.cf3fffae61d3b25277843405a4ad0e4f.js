128b63896fbc6eca52ed1de66ee4e9dfd9a24748
Taras Khomyn <taraskhomyn@MacBook-Air-Taras.local>
2020-09-15 20:08:17 +0300

    Updates

---
+++ precache-manifest.cf3fffae61d3b25277843405a4ad0e4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebe0beff802139d14de638f477ff282",
+    "url": "/HW17-timer/index.html"
+  },
+  {
+    "revision": "90b128e408729a6853ae",
+    "url": "/HW17-timer/static/css/main.95c218f1.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/HW17-timer/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW17-timer/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90b128e408729a6853ae",
+    "url": "/HW17-timer/static/js/main.bbf98b0b.chunk.js"
+  },
+  {
+    "revision": "7dd2e4fe89cf11635169",
+    "url": "/HW17-timer/static/js/runtime-main.882567a2.js"
+  }
+]);
